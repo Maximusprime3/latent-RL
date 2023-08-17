213dc82f1824bcc390aa3c670119abf78247a1e6
Max Frommknecht <Max.frommknecht@gmx.de>
2023-08-17 14:42:07 +0200

    doubl_loop

--- Code/VAE_RL/Efficient_double_loop.py
+++ Code/VAE_RL/Efficient_double_loop.py
@@ -42,10 +42,10 @@ def get_vae(version='version_0',log_directory='logs/BCE_test_VAE_1/MSSIMVAE/',
     vae = experiment.model
     return vae
 
-#Make a vectorized environment
+#Make a funciton to create environment, this allows to vectorize it
 def make_env(env_id: str = "MountainCarContinuous-v0", rank: int = 0, seed: int = 42, 
-            data_name: str = "test", collect_frames: bool = True, env_iterator: int = 0,
-            vae_version: int = 0, latent_dim: int = 2,
+            data_name: str = "test2", collect_frames: bool = True, env_iterator: int = 0,
+            vae_version: int = 0, latent_dim: int = 1,
             vae_directory: str = 'logs/MountainCar/BCE_test_VAE_1/MSSIMVAE/',
             hparam_path: str = "configs/bces_no_pretrained.yaml"):
     def _init():
@@ -106,7 +106,7 @@ def main():
     print("init")
     #collect episodes with random actions for picture data
     #no training
-    data_name='test2'
+    data_name='A2C_l1_test2'
     save_path='Data/MountainCar/'+data_name+'/'
     num_of_episodes = 1
 
@@ -137,7 +137,7 @@ def main():
 
 
     #Double loop vae and RL training
-    target_reward = 50 #90
+    target_reward = 80 #90
     current_reward = 0
     last_reward = -1000
     vae_version = 0
@@ -146,8 +146,8 @@ def main():
     train_new_vae = True # create the first agent only in the first round
     agent_model_dir = "RLmodels/MountainCarContinuous-v0/Double_loop"#where to save the RL agents
     agent_log_dir = agent_model_dir+"/logs" #where to log RL progress
-    vae_name = "Test1_A2C_vae_l2"
-    vae_directory = 'logs/MountainCar/BCE_test1_VAE_2/MSSIMVAE/' # directory for versions of the vae
+    vae_name = "BCE_VAE_l1_test2_A2C"
+    vae_directory = 'logs/MountainCar/BCE_VAE_1_test2/MSSIMVAE/' # directory for versions of the vae
 
     #num of resets
     n_rl_resets = -1
@@ -166,8 +166,9 @@ def main():
         print("num of collected obs", num_old_obs)
         num_new_obs = 0
 
+        ## do maybe 5% or soemething
         #do +1 to crack assymptotic improvements that never surpass a threshold, aka -1, -0.1, -0.01, -0.001 gets better but is also stuck
-        if current_reward <= last_reward + 1:
+        if current_reward <= last_reward * 0.95 :
             getting_worse += 1 
         else:
             getting_worse = 0
@@ -212,7 +213,7 @@ def main():
             #new agent from scratch 
             # Tuned
             print("making new rl agent")
-            n_steps = 100 
+            n_steps = 10 
             agent = A2C(
                 env = env,
                 n_steps= n_steps,           
@@ -282,7 +283,7 @@ def main():
         #         hparam_path = config_path) for i in range(n_envs)])
 
         eval_env = make_env(env_id = "MountainCarContinuous-v0", rank=0, 
-                data_name = "test", collect_frames = False,
+                data_name = "test2", collect_frames = False,
                 vae_version = vae_version,
                 vae_directory = vae_directory,
                 hparam_path = config_path)()
@@ -291,7 +292,7 @@ def main():
         
         #eval
         
-        n_eval_ep= int(num_of_steps/999)    #more training, more eval
+        n_eval_ep= int((num_of_steps/999)/2)    #more training, more eval
         if n_eval_ep < 10:                  #at least 10 episodes of eval
             n_eval_ep = 10 
         #mean_reward, std_reward = evaluate_policy(agent, eval_env, n_eval_episodes=10, deterministic=True)
@@ -303,8 +304,11 @@ def main():
         current_reward = mean_reward
 
         #save the model
-        
         agent.save("RLmodels/MountainCarContinuous-v0/Double_loop/end_of_loop_save/"+agent_name)
+        #save the rewards
+        np.savetxt('RLmodels/MountainCarContinuous-v0/Double_loop/rewards/'+agent_name+'_rewards.csv', reward_list, delimiter=',', fmt='%.4f')
+
+
         #count up vae version for naming
         vae_version += 1
         env_iter += 1 
